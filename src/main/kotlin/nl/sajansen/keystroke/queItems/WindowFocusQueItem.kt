63d831a9dd63a7a289eadbcc95f01141932eb92c
S. Jansen <sa.jansen1@gmail.com>
2020-05-26 11:43:53 +0200

    Extended window finding method

--- src/main/kotlin/nl/sajansen/keystroke/queItems/WindowFocusQueItem.kt
+++ src/main/kotlin/nl/sajansen/keystroke/queItems/WindowFocusQueItem.kt
@@ -1,12 +1,38 @@
 package nl.sajansen.keystroke.queItems
 
-import com.sun.jna.platform.DesktopWindow
-import com.sun.jna.platform.WindowUtils
+import com.sun.jna.Native
+import com.sun.jna.Pointer
 import com.sun.jna.platform.win32.User32
+import com.sun.jna.platform.win32.WinDef.HWND
+import com.sun.jna.platform.win32.WinUser.WNDENUMPROC
 import objects.notifications.Notifications
 import plugins.common.BasePlugin
 import plugins.common.QueItem
 import java.util.logging.Logger
+
+
+class WindowFinder(private val windowTitle: String) : WNDENUMPROC {
+    private val logger = Logger.getLogger(WindowFinder::class.java.name)
+
+    var windowHandle: HWND? = null
+    var count: Int = 0
+
+    override fun callback(hWnd: HWND, arg1: Pointer?): Boolean {
+        val windowText = CharArray(512)
+        User32.INSTANCE.GetWindowText(hWnd, windowText, 512)
+        val wText = Native.toString(windowText)
+
+        if (wText.isEmpty()) {
+            return true
+        }
+
+        if (wText.toLowerCase().contains(windowTitle)) {
+            windowHandle = hWnd
+            return false
+        }
+        return true
+    }
+}
 
 class WindowFocusQueItem(override val plugin: BasePlugin, override val name: String) : QueItem {
 
@@ -15,11 +41,11 @@ class WindowFocusQueItem(override val plugin: BasePlugin, override val name: Str
     override var executeAfterPrevious: Boolean = false
 
     override fun activate() {
-        val window = findWindowHandle(name) ?: return
+        val windowHandle = findWindowHandle(name) ?: return
 
-        logger.info("Set focus to window ${window.title}")
-        User32.INSTANCE.SetForegroundWindow(window.hwnd)
-        User32.INSTANCE.SetFocus(window.hwnd)
+        logger.info("Set focus to window $name")
+        User32.INSTANCE.SetForegroundWindow(windowHandle)
+        User32.INSTANCE.SetFocus(windowHandle)
     }
 
     override fun deactivate() {}
@@ -30,18 +56,18 @@ class WindowFocusQueItem(override val plugin: BasePlugin, override val name: Str
 
     override fun renderText(): String = "Focus: $name"
 
-    private fun findWindowHandle(windowTitle: String): DesktopWindow? {
+    private fun findWindowHandle(windowTitle: String): HWND? {
         if (!System.getProperty("os.name").toLowerCase().contains("win")) {
             logger.warning("Must run on Windows to find the window")
             Notifications.add("Plugin must run on Windows operating system", "EasyWorship")
             return null
         }
 
-        val window = WindowUtils.getAllWindows(true)
-            .find { it.title.contains(windowTitle) }
+        val windowFinder = WindowFinder(windowTitle.toLowerCase())
+        User32.INSTANCE.EnumWindows(windowFinder, null)
 
-        if (window != null) {
-            return window
+        if (windowFinder.windowHandle != null) {
+            return windowFinder.windowHandle
         }
 
         logger.warning("Failed to find window: $windowTitle")
